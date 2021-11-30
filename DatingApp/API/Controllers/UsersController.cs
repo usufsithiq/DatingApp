bd1a58366ec45b25f69654cb2e88899fed92c611
sithiq <usufsithiq2011@gmail.com>
2021-11-30 10:44:45 +0530

    End of section 4

--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -17,12 +17,14 @@ namespace API.Controllers
             _context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public ActionResult<IEnumerable<AppUser>> GetUsers()
         {
                 return _context.Users.ToList();
         }
 
+        [Authorize]
         [HttpGet("{id}")]
         public ActionResult<AppUser> GetUser(int id)
         {
